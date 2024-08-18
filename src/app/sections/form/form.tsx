f989515bb5711969511f6ee76b9047673e99edf4
AdrifeCova <adrife2000@hotmail.com>
2024-08-18 10:48:07 -0600

    feat: finish adding the Scheam from the client side

--- src/app/sections/form/form.tsx
+++ src/app/sections/form/form.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import stylesForm from './form.module.css'
 import { useState } from 'react'
 import React from 'react'
+import { UiValidator, UiValidatorErrors } from '@uireact/validator';
+
 
 
 export const Form = () => {
@@ -15,48 +17,80 @@ export const Form = () => {
   const [isFocusName, setIsFocusName] = useState(false);
   const [isFocusEmail, setIsFocusEmail] = useState(false);
   const [isFocusMessage, setIsFocusMessage] = useState(false);
+  const [onSubmit, setOnSubmit] = useState(false);
+
+  //  Validate from client to then sent to API (using UiReact)
+  const validator = new UiValidator();
 
 
-
-
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setContactInfo({...contactInfo, [e.target.name] : [e.target.value]})
+  const Schema = {
+    name: validator.ruler().isRequired('first Name is required'),
+    email: validator.ruler().isRequired('email is required'),
+    message: validator.ruler().isRequired('message is required'),
   }
 
-  // const submitContact = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   e.preventDefault()
-  //   const {name, email, message} = contactInfo;
-  //   const newContact = {
-  //     name,
-  //     email,
-  //     message
-  //   }
+  const checkValidation = (e: React.FormEvent) => {
+    e.preventDefault()
+    const { name, email, message } = contactInfo;
 
-  //   console.log(newContact);
-  // }
+    const newContact = {
+      name,
+      email,
+      message
+    }
+
+    const result = validator.validate(Schema, newContact);
+
+    if (result.passed) {
+        setOnSubmit(true)
+        contactInfo.name = '';
+        contactInfo.email = '';
+        contactInfo.message = '';
+        setIsFocusName(false);
+        setIsFocusEmail(false);
+        setIsFocusMessage(false);
+        console.log(newContact);
+    } else {
+     return console.log(result.errors);
+    }
+  }
+
+
+
+
+  const onChangeTextAreas = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContactInfo({...contactInfo, [e.currentTarget.name] : [e.currentTarget.value]})
+  }
+
+  const onChangeInputs = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContactInfo({...contactInfo, [e.currentTarget.name] : [e.currentTarget.value]})
+  }
+
 
 
   return (
     <div className={stylesForm.container}>
-      <form className={stylesForm.form}>
+      <form onSubmit={checkValidation} className={stylesForm.form}>
         <h3 className={stylesForm.title}>
           NewsLetter
         </h3>
         <div onFocus={() => setIsFocusName(true)} className={stylesForm.wrapper}>
-          <input type="text" name='name' id='name' value={contactInfo.name} className={stylesForm.input} onChange={onChange} required autoComplete='off'/>
-          <label className={isFocusName ? stylesForm.active  :  stylesForm.label} htmlFor='name'>Full Name</label>
+          <input type="text" name='name' id='name' value={contactInfo.name} className={stylesForm.input} onChange={onChangeInputs}  autoComplete='off' required/>
+          <label className={isFocusName ? stylesForm.active : stylesForm.label} htmlFor='name'>Full Name</label>
+          <span className={stylesForm.span}>This is an error message</span>
         </div>
         <div onFocus={() => setIsFocusEmail(true)} className={stylesForm.wrapper}>
-          <input type="text" name='email' value={contactInfo.email} id='email' className={stylesForm.input} onChange={onChange} required autoComplete='off' />
+          <input type="email" name='email' value={contactInfo.email} id='email' className={stylesForm.input} onChange={onChangeInputs}  autoComplete='off' required />
           <label className={isFocusEmail ? stylesForm.active  :  stylesForm.label} htmlFor='email'>Email</label>
+          <span className={stylesForm.span}>This is an error message</span>
         </div>
         <div onFocus={() => setIsFocusMessage(true)} className={stylesForm.wrapper}>
-          <textarea  className={stylesForm.input} name='message' id='message' value={contactInfo.message}  autoComplete='off' required></textarea>
+          <textarea  className={stylesForm.input} name='message' id='message' value={contactInfo.message}  autoComplete='off' onChange={onChangeTextAreas} ></textarea>
           <label className={isFocusMessage ? stylesForm.active  :  stylesForm.label} htmlFor='message'>Message</label>
+          <span className={stylesForm.span}>This is an error message</span>
         </div>
         <div className={stylesForm.flex}>
-          <button type="submit" className={stylesForm.btn}>Send</button>
+          <button type="submit" className={stylesForm.btn}>{ onSubmit ? `Sending...`: 'Send'}</button>
           <Link href='/' className={stylesForm.btn}>Go Home</Link>
         </div>
       </form>
