07d7ba414b4ae612d38e9767d2ade89275ff956e
AdrifeCova <adrife2000@hotmail.com>
2024-09-22 19:41:27 -0600

    chore: making the app works :8

--- apps/web/src/app/loading.tsx
+++ apps/web/src/app/loading.tsx
@@ -4,6 +4,7 @@ const LoadingPage = ({isActive = false}) => {
   return (
     <div className={ isActive ? stylesLoading.loaderActive : stylesLoading.loader}>
       <div className={isActive ? stylesLoading.spinnerActive : stylesLoading.spinner}></div>
+    s
     </div>
   );
 };
